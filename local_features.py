9ef789e2aa80a33a03b88df12efa19d031a0e0c0
hailstonem <martin@martins-mbp-2.home>
2016-09-16 14:44:54 +0100

    Tidied and commented feature creation

--- local_features.py
+++ local_features.py
@@ -3,7 +3,10 @@
 Created on Wed Mar 30 14:25:35 2016
 
 @author: martin
+
 Feature calculation methods (moved from v2_functions for clarity)
+ makes adding new features more straightforward
+ might be nice to add subclasses to this
 """
 import vigra
 import scipy
@@ -15,6 +18,7 @@ from skimage import morphology
 from sklearn import linear_model
 from sklearn import preprocessing
 import scipy.ndimage as ndimage
+import scipy.signal as signal
 import skimage
 import time
 import numpy as np
@@ -23,36 +27,19 @@ import threading
 from scipy.ndimage.interpolation import shift
 
 
-def feature_create_threadable_auto(par_obj,imno,tpt,zslice):
-    [feat_length,feat_func]=get_feature_lengths(par_obj.feature_type)
-    feat=feat_func(par_obj.data_store['pred_arr'][imno][tpt][zslice],par_obj.feature_scale)
-    return feat
-def auto_context_features(feat_array):
-    #pattern=[1,2,3,5,7,10,12,15,20,25,30,35,40,45,50,60,70,80,90,100]
-    pattern=[1,2,4,8,16,32,64,128]
-    feat_list=[]
-    for xshift in pattern:
-        for yshift in pattern:
-            a=shift(feat_array, (xshift,0),order=0, cval=0)
-            b=shift(feat_array, (-xshift,0),order=0, cval=0)
-            c=shift(feat_array, (yshift,0),order=0, cval=0)
-            d=shift(feat_array, (-yshift,0),order=0, cval=0)
-            feat_list.append(a)
-            feat_list.append(b)
-            feat_list.append(c)
-            feat_list.append(d)
-    return feat_list
 
-    
 def get_feature_lengths(feature_type):
     #dictionary of feature sets
     #can add arbitrary feature sets by defining a name, length, and function that accepts two arguments
-    feature_dict={'basic': [13,local_shape_features_basic2],
+    feature_dict={'basic': [13,local_shape_features_basic],
                   'fine': [21,local_shape_features_fine],
+                  'daisy': [53,local_shape_features_daisy],
                   'fine3': [26,local_shape_features_fine3],
                   'texton': [21,local_shape_features_texton],
                   'comb': [51, local_shape_features_fine_comb],
+                  'dual': [36, local_shape_features_dual],
                   'normal': [26, local_shape_features_fine3_normal],
+                  'median': [26, local_shape_features_fine3_median],
                   'patch': [45,local_shape_features_patch] }
     feat_length=None
     if feature_dict.has_key(feature_type):
@@ -64,6 +51,7 @@ def get_feature_lengths(feature_type):
     return feat_length,feat_func
 
 def feature_create_threadable(par_obj,imRGB):
+    
     time1 = time.time()
     [feat_length,feat_func]=get_feature_lengths(par_obj.feature_type)
     feat = np.zeros(((int(par_obj.crop_y2)-int(par_obj.crop_y1)),(int(par_obj.crop_x2)-int(par_obj.crop_x1)),feat_length*(par_obj.ch_active.__len__())))
@@ -82,6 +70,9 @@ def feature_create_threadable(par_obj,imRGB):
     return feat
     
 def feature_create_z(par_obj,):
+    # calculates simple Z based features
+    # somewhat buggy
+    # little benefit from these features
     import v2
     #checks if features already in array
     if zslice not in par_obj.data_store['feat_arr'][imno][tpt]:
@@ -112,6 +103,30 @@ def feature_create_z(par_obj,):
 
     return feat
 
+
+def feature_create_threadable_auto(par_obj,imno,tpt,zslice):
+    #allows auto-context based features to be included
+    #currently slow, could optimise to calculate more efficiently
+    [feat_length,feat_func]=get_feature_lengths(par_obj.feature_type)
+    feat=feat_func(par_obj.data_store['pred_arr'][imno][tpt][zslice],par_obj.feature_scale)
+    return feat
+def auto_context_features(feat_array):
+    #pattern=[1,2,3,5,7,10,12,15,20,25,30,35,40,45,50,60,70,80,90,100]
+    pattern=[1,2,4,8,16,32,64,128]
+    feat_list=[]
+    for xshift in pattern:
+        for yshift in pattern:
+            a=shift(feat_array, (xshift,0),order=0, cval=0)
+            b=shift(feat_array, (-xshift,0),order=0, cval=0)
+            c=shift(feat_array, (yshift,0),order=0, cval=0)
+            d=shift(feat_array, (-yshift,0),order=0, cval=0)
+            feat_list.append(a)
+            feat_list.append(b)
+            feat_list.append(c)
+            feat_list.append(d)
+    return feat_list
+
+    
 def local_shape_features_fine(im,scaleStart):
     #Exactly as in the Luca Fiaschi paper.
     s = scaleStart
@@ -150,8 +165,9 @@ def local_shape_features_fine(im,scaleStart):
     return f
     
 def local_shape_features_fine3_1(im,scaleStart):
-    #Exactly as in the Luca Fiaschi paper.
-    #FIXME normalisation. Broke comparing image, implement based on max datatype stored instead
+    # Uses gaussian pyramid to calculate at multiple scales
+    # Smoothing and scale parameters chosen to approximate Luca Fiashi paper
+    #FIXME normalisation. Comparing image broken?? due to scaling, implement based on max datatype stored instead
     pyr_levels=5
     
     scale_mode='nearest'
@@ -183,9 +199,56 @@ def local_shape_features_fine3_1(im,scaleStart):
         f[:,:, layer*4+4]  = st1
 
     return f
-def local_shape_features_fine3_normal(im,scaleStart):
-    #Exactly as in the Luca Fiaschi paper.
+def local_shape_features_fine3_median(im,scaleStart):
+    # Uses gaussian pyramid to calculate at multiple scales
+    # Smoothing and scale parameters chosen to approximate Luca Fiashi paper
     #FIXME normalisation. Broke comparing image, implement based on max datatype stored instead
+    s = scaleStart
+    
+    imSizeC = im.shape[0]
+    imSizeR = im.shape[1]
+    f = np.zeros((imSizeC,imSizeR,26))
+    a=im
+    #median based scaling (inefficient to do this to every image)
+    #TODO subsample whole file, then normalise
+    scaled=preprocessing.robust_scale(a)/1000
+    a=scaled.reshape(a.shape)
+    pyr=skimage.transform.pyramid_gaussian(a,sigma=1.5, max_layer=5, downscale=2)
+    f[:,:, 0]  = a
+    for layer in range(0,5):
+        scale=[float(im.shape[0])/float(a.shape[0]),float(im.shape[1])/float(a.shape[1])]
+        lap=scipy.ndimage.filters.laplace(a)
+        lap=scipy.ndimage.interpolation.zoom(lap, scale,order=1)
+        
+        [m,n]=np.gradient(a)
+        ggm=np.hypot(m,n)
+        ggm=scipy.ndimage.interpolation.zoom(ggm, scale,order=1)
+
+        x,y,z=skfeat.structure_tensor(a,1)
+        st =skfeat.structure_tensor_eigvals(x,y,z)
+        st0=scipy.ndimage.interpolation.zoom(st[0], scale,order=1)
+        st1=scipy.ndimage.interpolation.zoom(st[1], scale,order=1)
+
+        #ent=entropy(a,skimage.morphology.disk(3))
+        ent=scipy.ndimage.interpolation.zoom(a, scale,order=1)
+        
+        #hess=vigra.filters.hessianOfGaussianEigenvalues(a.astype('float32'),2)
+        #hess0=scipy.ndimage.interpolation.zoom(hess[:,:,0], scale,order=1,mode='nearest')
+        #hess1=scipy.ndimage.interpolation.zoom(hess[:,:,1], scale,order=1,mode='nearest')
+        f[:,:, layer*5+1]  = lap
+        f[:,:, layer*5+2]  = ggm
+        f[:,:, layer*5+3]  = st0
+        f[:,:, layer*5+4]  = st1
+        f[:,:, layer*5+5]  = ent
+        a=pyr.next()
+    return f
+    
+def local_shape_features_fine3_normal(im,scaleStart):
+    # Uses gaussian pyramid to calculate at multiple scales
+    # Smoothing and scale parameters chosen to approximate Luca Fiashi paper
+    # FIXME normalisation. Broke comparing image, implement based on max datatype stored instead. DONE?
+    # Vector based normalisation for each feature.
+    # Worse than without normalisation
     s = scaleStart
     
     imSizeC = im.shape[0]
@@ -235,7 +298,9 @@ def local_shape_features_fine3_normal(im,scaleStart):
         a=pyr.next()
     return f
 def local_shape_features_fine3(im,scaleStart):
-    #Exactly as in the Luca Fiaschi paper.
+    
+    # Uses gaussian pyramid to calculate at multiple scales
+    # Smoothing and scale parameters chosen to approximate Luca Fiashi paper
     #FIXME normalisation. Broke comparing image, implement based on max datatype stored instead
     s = scaleStart
     
@@ -246,6 +311,7 @@ def local_shape_features_fine3(im,scaleStart):
     #im=exposure.equalize_adapthist(im, kernel_size=5)
     pyr=skimage.transform.pyramid_gaussian(im,sigma=1.5, max_layer=5, downscale=2)
     a=im
+
     for layer in range(0,5):
         scale=[float(im.shape[0])/float(a.shape[0]),float(im.shape[1])/float(a.shape[1])]
         lap=scipy.ndimage.filters.laplace(a)
@@ -262,6 +328,91 @@ def local_shape_features_fine3(im,scaleStart):
 
         #ent=entropy(a,skimage.morphology.disk(3))
         ent=scipy.ndimage.interpolation.zoom(a, scale,order=1)
+        
+        #hess=vigra.filters.hessianOfGaussianEigenvalues(a.astype('float32'),2)
+        #hess0=scipy.ndimage.interpolation.zoom(hess[:,:,0], scale,order=1,mode='nearest')
+        #hess1=scipy.ndimage.interpolation.zoom(hess[:,:,1], scale,order=1,mode='nearest')
+        f[:,:, layer*5+1]  = lap
+        f[:,:, layer*5+2]  = ggm
+        f[:,:, layer*5+3]  = st0
+        f[:,:, layer*5+4]  = st1
+        f[:,:, layer*5+5]  = ent
+        a=pyr.next()
+    return f
+def local_shape_features_daisy(im,scaleStart):
+    # skiimage daisy
+    # fast SIFT-like features
+    # still slow
+    from skimage.feature import daisy
+
+    imSizeC = im.shape[0]
+    imSizeR = im.shape[1]
+    f = np.zeros((imSizeC,imSizeR,53))
+    f[:,:, 0]  = im
+    #im=exposure.equalize_adapthist(im, kernel_size=5)
+    a=im
+    daisyfeat= daisy(a,histograms=4, orientations=4)
+    scale=[float(im.shape[0])/float(daisyfeat.shape[0]),float(im.shape[1])/float(daisyfeat.shape[1])]  
+    for i in range(daisyfeat.shape[2]):
+        f[:,:, i+1]=scipy.ndimage.interpolation.zoom(daisyfeat[:,:,i],scale,order=1)
+    return f
+    
+def local_shape_features_fine3_cust(im,scaleStart):
+    # Added custom feature to test for improvement in NB detection
+    # results minimal
+    s = scaleStart
+    
+    imSizeC = im.shape[0]
+    imSizeR = im.shape[1]
+    f = np.zeros((imSizeC,imSizeR,26))
+    f[:,:, 0]  = im
+    #im=exposure.equalize_adapthist(im, kernel_size=5)
+    pyr=skimage.transform.pyramid_gaussian(im,sigma=1.5, max_layer=5, downscale=2)
+    a=im
+    cust_feat=np.asarray([[77,78,78,74,76,81,82,82,81,84,79,81,85,83,76,76,86,88,86,86,93,95,91,88,80,77],
+    [80,81,83,82,81,79,77,79,81,82,74,74,86,87,80,85,91,90,91,92,96,96,94,88,81,78],
+    [88,88,90,87,83,79,79,82,86,85,79,78,86,90,87,93,95,95,93,94,94,93,95,90,83,78],
+    [91,94,95,91,86,85,86,87,90,87,80,81,89,95,90,90,97,100,93,89,89,92,91,87,82,74],
+    [95,96,93,92,88,89,90,89,88,80,75,73,83,90,83,79,87,94,91,89,88,88,86,83,81,76],
+    [93,96,94,89,88,92,92,85,80,70,64,61,64,71,66,63,75,89,88,89,92,89,85,79,79,81],
+    [86,92,94,89,89,89,86,77,67,58,56,53,50,48,48,51,61,77,82,86,92,90,86,79,77,82],
+    [86,91,93,93,91,88,82,70,57,49,47,45,42,39,39,41,47,59,70,77,85,89,87,82,79,82],
+    [88,90,95,100,94,89,77,59,48,41,39,38,38,35,35,35,40,48,57,67,80,88,90,86,81,81],
+    [86,91,95,97,87,75,61,47,40,36,37,38,39,38,37,34,36,41,49,58,70,80,87,85,82,84],
+    [76,85,93,90,79,63,51,41,35,34,41,44,42,40,42,41,37,39,47,56,64,75,80,79,74,79],
+    [76,80,87,90,83,66,48,39,35,37,42,45,45,43,45,44,38,38,45,53,61,73,81,78,74,81],
+    [83,87,90,95,90,71,48,39,35,38,40,43,47,47,45,42,39,38,42,50,64,83,89,86,86,85],
+    [85,86,86,89,83,64,50,42,38,39,42,45,47,47,43,40,38,35,39,48,71,90,95,90,87,83],
+    [81,74,78,81,73,61,53,45,38,38,44,45,43,45,45,42,37,35,39,48,66,83,90,87,80,76],
+    [79,74,79,80,75,64,56,47,39,37,41,42,40,42,44,41,34,35,41,51,63,79,90,93,85,76],
+    [84,82,85,87,80,70,58,49,41,36,34,37,38,39,38,37,36,40,47,61,75,87,97,95,91,86],
+    [81,81,86,90,88,80,67,57,48,40,35,35,35,38,38,39,41,48,59,77,89,94,100,95,90,88],
+    [82,79,82,87,89,85,77,70,59,47,41,39,39,42,45,47,49,57,70,82,88,91,93,93,91,86],
+    [82,77,79,86,90,92,86,82,77,61,51,48,48,50,53,56,58,67,77,86,89,89,89,94,92,86],
+    [81,79,79,85,89,92,89,88,89,75,63,66,71,64,61,64,70,80,85,92,92,88,89,94,96,93],
+    [76,81,83,86,88,88,89,91,94,87,79,83,90,83,73,75,80,88,89,90,89,88,92,93,96,95],
+    [74,82,87,91,92,89,89,93,100,97,90,90,95,89,81,80,87,90,87,86,85,86,91,95,94,91],
+    [78,83,90,95,93,94,94,93,95,95,93,87,90,86,78,79,85,86,82,79,79,83,87,90,88,88],
+    [78,81,88,94,96,96,92,91,90,91,85,80,87,86,74,74,82,81,79,77,79,81,82,83,81,80],
+    [77,80,88,91,95,93,86,86,88,86,76,76,83,85,81,79,84,81,82,82,81,76,74,78,78,77]])
+    for layer in range(0,5):
+        scale=[float(im.shape[0])/float(a.shape[0]),float(im.shape[1])/float(a.shape[1])]
+        lap=scipy.ndimage.filters.laplace(a)
+        lap=scipy.ndimage.interpolation.zoom(lap, scale,order=1)
+        
+        [m,n]=np.gradient(a)
+        ggm=np.hypot(m,n)
+        ggm=scipy.ndimage.interpolation.zoom(ggm, scale,order=1)
+
+        x,y,z=skfeat.structure_tensor(a,1)
+        st =skfeat.structure_tensor_eigvals(x,y,z)
+        st0=scipy.ndimage.interpolation.zoom(st[0], scale,order=1)
+        st1=scipy.ndimage.interpolation.zoom(st[1], scale,order=1)
+
+        #ent=entropy(a,skimage.morphology.disk(3))
+        ent=signal.convolve2d(a,cust_feat,mode='same')
+        ent=scipy.ndimage.interpolation.zoom(ent, scale,order=1)
+        
         #hess=vigra.filters.hessianOfGaussianEigenvalues(a.astype('float32'),2)
         #hess0=scipy.ndimage.interpolation.zoom(hess[:,:,0], scale,order=1,mode='nearest')
         #hess1=scipy.ndimage.interpolation.zoom(hess[:,:,1], scale,order=1,mode='nearest')
@@ -274,8 +425,8 @@ def local_shape_features_fine3(im,scaleStart):
     return f
     
 def local_shape_features_fine_comb(im,scaleStart):
-    #Exactly as in the Luca Fiaschi paper.
-    #FIXME normalisation. Broke comparing image, implement based on max datatype stored instead
+    # Combinatorial features
+    # based on idea that RF struggles with complex decision boundaries
     s = scaleStart
     
     imSizeC = im.shape[0]
@@ -319,7 +470,8 @@ def local_shape_features_fine_comb(im,scaleStart):
         a=pyr.next()
     return f
 def local_shape_features_finez(im,scaleStart):
-    #Exactly as in the Luca Fiaschi paper.
+    # Allows passing of a volume
+    # Little improvement, for obvious reasons wrt lack of 3D information
     #FIXME normalisation. Broke comparing image, implement based on max datatype stored instead
     s = scaleStart
     
@@ -354,8 +506,58 @@ def local_shape_features_finez(im,scaleStart):
         f[:,:, layer*5+5]  = ent
         a=pyr.next()
     return f
+def local_shape_features_dual(im,scaleStart):
+    # include bar and edge detector filters for texture
+    # based on MR8 filterbank
+    s = scaleStart
+    
+    imSizeC = im.shape[0]
+    imSizeR = im.shape[1]
+    f = np.zeros((imSizeC,imSizeR,26+2*5))
+    f[:,:, 0]  = im
+    #im=exposure.equalize_adapthist(im, kernel_size=5)
+    pyr=skimage.transform.pyramid_gaussian(im,sigma=1.5, max_layer=5, downscale=2)
+    n_orientations=6
+    edge, bar, rot=makeRFSfilters(radius=10, sigmas=[1], n_orientations=n_orientations)
+    a=im
+    for layer in range(0,5):
+        scale=[float(im.shape[0])/float(a.shape[0]),float(im.shape[1])/float(a.shape[1])]
+        lap=scipy.ndimage.filters.laplace(a)
+        lap=scipy.ndimage.interpolation.zoom(lap, scale,order=1)
+        
+        [m,n]=np.gradient(a)
+        ggm=np.hypot(m,n)
+        ggm=scipy.ndimage.interpolation.zoom(ggm, scale,order=1)
+
+        x,y,z=skfeat.structure_tensor(a,1)
+        st =skfeat.structure_tensor_eigvals(x,y,z)
+        st0=scipy.ndimage.interpolation.zoom(st[0], scale,order=1)
+        st1=scipy.ndimage.interpolation.zoom(st[1], scale,order=1)
+
+        #ent=entropy(a,skimage.morphology.disk(3))
+        ent=scipy.ndimage.interpolation.zoom(a, scale,order=1)
+        #hess=vigra.filters.hessianOfGaussianEigenvalues(a.astype('float32'),2)
+        #hess0=scipy.ndimage.interpolation.zoom(hess[:,:,0], scale,order=1,mode='nearest')
+        #hess1=scipy.ndimage.interpolation.zoom(hess[:,:,1], scale,order=1,mode='nearest')
+        barmax=edgemax=np.zeros(a.shape)
+        for orient in range(n_orientations):
+            edgemax = np.maximum(edgemax,scipy.ndimage.convolve(a,edge[0,orient,:,:]))
+            barmax = np.maximum(edgemax,scipy.ndimage.convolve(a,bar[0,orient,:,:]))
+        edgemax=scipy.ndimage.interpolation.zoom(edgemax, scale,order=1)     
+        barmax=scipy.ndimage.interpolation.zoom(barmax, scale,order=1)
+        
+        f[:,:, layer*5+1]  = lap
+        f[:,:, layer*5+2]  = ggm
+        f[:,:, layer*5+3]  = st0
+        f[:,:, layer*5+4]  = st1
+        f[:,:, layer*5+5]  = ent
+        f[:,:, layer*5+6]  = edgemax
+        f[:,:, layer*5+7]  = barmax
+        a=pyr.next()
+    return f
 def local_shape_features_texton(im,scaleStart):
     #FIXME normalisation. Broke comparing image, implement based on max datatype stored instead
+    # based on MR8 filterbank
     pyr_levels=5
     
     s = scaleStart
@@ -397,7 +599,8 @@ def local_shape_features_texton(im,scaleStart):
     return f
     
 def local_shape_features_patch(im,scaleStart):
-    #FIXME normalisation. Broke comparing image, implement based on max datatype stored instead
+    # RAW multiscale patches
+    # Learns slowly and reaches limit quickly
     pyr_levels=5
     #check pyramid for starting point and if loops match up correctly
     s = scaleStart
@@ -526,47 +729,10 @@ def local_shape_features_basic(im,scaleStart):
     f[:,:, 12] = vigra.filters.laplacianOfGaussian(im, s*4 ,window_size=2.5)
     return f
 
-def local_shape_features_basic2(im,scaleStart):
-    #Exactly as in the Luca Fiaschi paper.
-    s = scaleStart
-    
-    imSizeC = im.shape[0]
-    imSizeR = im.shape[1]
-    f = np.zeros((imSizeC,imSizeR,13))
 
-    #st08 = vigra.filters.structureTensorEigenvalues(im,s*1,s*2)
-    x,y,z=skfeat.structure_tensor(im,s*1)
-    st08 =skfeat.structure_tensor_eigvals(x,y,z)
-    #st16 = vigra.filters.structureTensorEigenvalues(im,s*2,s*4)
-    x,y,z=skfeat.structure_tensor(im,s*2)
-    st16 =skfeat.structure_tensor_eigvals(x,y,z)
-    #st32 = vigra.filters.structureTensorEigenvalues(im,s*4,s*8)
-    x,y,z=skfeat.structure_tensor(im,s*4)
-    st32 = skfeat.structure_tensor_eigvals(x,y,z)
-
-    f[:,:, 0]  = im
-    f[:,:, 1]  = ndimage.gaussian_gradient_magnitude(im,s,truncate=2.5)
-
-    f[:,:, 2]  = st08[0]
-    f[:,:, 3]  = st08[1]
-    f[:,:, 4]  = ndimage.gaussian_laplace(im,s,truncate=2.5)
-
-    f[:,:, 5]  =ndimage.gaussian_gradient_magnitude(im,s*2,truncate=2.5)
-
-    f[:,:, 6]  = st08[0]
-    f[:,:, 7]  = st08[1]
-
-    f[:,:, 8]  = ndimage.gaussian_laplace(im,s*2,truncate=2.5)
-
-    f[:,:, 9]  = ndimage.gaussian_gradient_magnitude(im,s*4,truncate=2.5)
-
-    f[:,:, 10]  = st08[0]
-    f[:,:, 11]  = st08[1]
-
-    f[:,:, 12] = ndimage.gaussian_laplace(im,s*4,truncate=2.5)
-    return f
 def local_shape_features_basic3(im,scaleStart):
     #Exactly as in the Luca Fiaschi paper.
+    #clunky multiscale version
     intp='bilinear' 
     s = scaleStart
     pyr=skimage.transform.pyramid_gaussian(im,sigma=1.5, max_layer=4, downscale=2)
