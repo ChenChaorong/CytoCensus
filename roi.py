5db25b4cd58dd7c67bc7963c7f30fef09d99e858
hailstonem <martin@martins-mbp-3.home>
2017-07-09 22:35:33 +0100

    PEP-8 Whitespace removed

--- roi.py
+++ roi.py
@@ -8,7 +8,7 @@ sys.path.append('final_software')
 import v2_functions as v2
 import numpy as np
 
-  
+
 
 
 
@@ -23,32 +23,32 @@ class Win_fn(QtGui.QWidget):
         self.par_obj = par_obj
 
 
-        
+
     def show_image(self):
 
-       
-        
+
+
         self.figure1 = Figure(figsize=(8, 8), dpi=100)
         self.canvas1 = FigureCanvas(self.figure1)
         self.figure1.patch.set_facecolor('grey')
-        
+
         self.plt1 = self.figure1.add_subplot(1, 1, 1)
         self.plt1.set_title("Title")
         #self.plt1.set_xlim([0,100])
         #self.plt1.set_ylim([0,100])
-        
+
         self.cursor = v2.ROI(self,par_obj)
-        
-        
+
+
         im_RGB = np.zeros((512, 512))
         #Makes sure it spans the whole figure.
         #self.figure1.subplots_adjust(left=0.001, right=0.999, top=0.999, bottom=0.001)
-        
+
         self.plt1.imshow(par_obj.ex_img)
         self.plt1.set_ylim(0,par_obj.ex_img.shape[0])
         self.plt1.set_xlim(0,par_obj.ex_img.shape[1])
         self.canvas1.show()
-        
+
         self.top_panel = QtGui.QHBoxLayout()
         self.compl_btn = QtGui.QPushButton('complete')
         self.compl_btn.clicked.connect(self.cursor.complete_roi)
@@ -64,7 +64,7 @@ class Win_fn(QtGui.QWidget):
         self.top_panel.addWidget(self.next_btn)
         self.top_panel.addWidget(self.interpolate_btn)
         self.top_panel.addStretch()
-        
+
         vbox = QtGui.QVBoxLayout()
         self.setLayout(vbox)
         vbox.addLayout(self.top_panel)
@@ -72,9 +72,9 @@ class Win_fn(QtGui.QWidget):
 
 
 
-        
-        
-        
+
+
+
 
         self.canvas1.mpl_connect('motion_notify_event', self.cursor.motion_notify_callback)
         self.canvas1.mpl_connect('button_press_event', self.cursor.button_press_callback)
@@ -86,7 +86,7 @@ class Win_fn(QtGui.QWidget):
             par_obj.prev_img = par_obj.curr_img
             par_obj.curr_img = im_num
             self.goto_img_fn(im_num,self.par_obj, self)
-            
+
     def next_im_btn_fn(self):
         im_num = par_obj.curr_img + 1
         if im_num <par_obj.test_im_end:
@@ -102,17 +102,17 @@ class Win_fn(QtGui.QWidget):
             frames =par_obj.frames_2_load[b]
             for i in frames:
                 count = count+1
-                
+
                 if par_obj.curr_img == count:
                     break;
             else:
-                continue 
-            break 
-        
+                continue
+            break
 
-        
+
+
         imRGB = par_obj.oib_file[0,count,:,:]
-        
+
     #            if event.button == 3:
         self.plt1.cla()
         self.plt1.imshow(imRGB)
@@ -123,7 +123,7 @@ class Win_fn(QtGui.QWidget):
         self.cursor.line = [None]
         self.cursor.draw_ROI()
         self.canvas1.draw()
-        
+
         self.cursor.complete = False
         self.cursor.flag = False
         for bt in self.par_obj.roi_stk_x:
@@ -131,12 +131,12 @@ class Win_fn(QtGui.QWidget):
                 self.cursor.complete = True
                 self.cursor.ppt_x = copy.deepcopy(self.par_obj.roi_stk_x[bt])
                 self.cursor.ppt_y = copy.deepcopy(self.par_obj.roi_stk_y[bt])
-                
-                break;
-        
-            
 
-        
+                break;
+
+
+
+
 class parameterClass:
     def __init__(self):
         self.file_array = ['/Users/dwaithe/Documents/collaborators/YangLu/working_examples/Genotype_wt_syp_Staiining_Ase_Dpn/20150206_DAPI_Dpn568_Ase647_60x_4xZoom_20Stacks_WT04.oib']
@@ -147,15 +147,15 @@ class parameterClass:
         self.roi_stkint_x ={}
         self.roi_stkint_y ={}
         self.npts = 500
-        
-    
+
+
 
 if __name__ == "__main__":
     app = QtGui.QApplication([])
     app.processEvents()
     par_obj = parameterClass()
     success, updateText = v2.import_data_fn(par_obj, par_obj.file_array)
-    
+
     #par_obj.frames_2_load = [range(0,par_obj.dv_file.maxFrames)]
     par_obj.frames_2_load = [range(0,par_obj.oib_file.shape[1])]
 
@@ -167,7 +167,7 @@ if __name__ == "__main__":
 
     print success
     win_tab.show()
-    
-      
+
+
     if (sys.flags.interactive != 1) or not hasattr(QtCore, 'PYQT_VERSION'):
-        QtGui.QApplication.instance().exec_() 
+        QtGui.QApplication.instance().exec_()
