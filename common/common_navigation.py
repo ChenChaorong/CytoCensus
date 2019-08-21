86b30beb1461a72c8613ec51359ee060e4ba8248
M <hailstonem@users.noreply.github.com>
2019-08-21 17:42:59 +0100

    Black formatting

--- common/common_navigation.py
+++ common/common_navigation.py
@@ -21,41 +21,41 @@
 """
 from PyQt5 import QtGui, QtCore, QtWidgets
 from gnu import return_license
-import numpy as np
 from functions.v2_functions import return_rgb_slice
 
-def navigation_setup(self,par_obj):
 
-    #Sets up the button which changes to the prev image
-    self.prev_im_btn = QtWidgets.QPushButton('Z ('+chr(8595)+')')
+def navigation_setup(self, par_obj):
+
+    # Sets up the button which changes to the prev image
+    self.prev_im_btn = QtWidgets.QPushButton("Z (" + chr(8595) + ")")
     self.prev_im_btn.setEnabled(True)
 
-    #Sets up the button which changes to the next Image.
-    self.next_im_btn = QtWidgets.QPushButton('Z ('+chr(8593)+')')
+    # Sets up the button which changes to the next Image.
+    self.next_im_btn = QtWidgets.QPushButton("Z (" + chr(8593) + ")")
     self.next_im_btn.setEnabled(True)
 
-    #Sets up the button which changes to the prev image
-    self.first_im_btn = QtWidgets.QPushButton('Z (bottom)')
+    # Sets up the button which changes to the prev image
+    self.first_im_btn = QtWidgets.QPushButton("Z (bottom)")
     self.first_im_btn.setEnabled(True)
 
-    #Sets up the button which changes to the next Image.
-    self.last_im_btn = QtWidgets.QPushButton('Z (top)')
+    # Sets up the button which changes to the next Image.
+    self.last_im_btn = QtWidgets.QPushButton("Z (top)")
     self.last_im_btn.setEnabled(True)
 
-     #Sets up the button which changes to the prev image
-    self.prev_time_btn = QtWidgets.QPushButton('Time ('+chr(8592)+')')
+    # Sets up the button which changes to the prev image
+    self.prev_time_btn = QtWidgets.QPushButton("Time (" + chr(8592) + ")")
     self.prev_time_btn.setEnabled(True)
 
-    #Sets up the button which changes to the next Image.
-    self.next_time_btn = QtWidgets.QPushButton('Time ('+chr(8594)+')')
+    # Sets up the button which changes to the next Image.
+    self.next_time_btn = QtWidgets.QPushButton("Time (" + chr(8594) + ")")
     self.next_time_btn.setEnabled(True)
 
-    #Sets up the button which changes to the next File.
-    self.prev_file_btn = QtWidgets.QPushButton('File (<)')
+    # Sets up the button which changes to the next File.
+    self.prev_file_btn = QtWidgets.QPushButton("File (<)")
     self.prev_file_btn.setEnabled(True)
 
-    #Sets up the button which changes to the next File.
-    self.next_file_btn = QtWidgets.QPushButton('File (>)')
+    # Sets up the button which changes to the next File.
+    self.next_file_btn = QtWidgets.QPushButton("File (>)")
     self.next_file_btn.setEnabled(True)
 
     self.output_count_txt = QtWidgets.QLabel()
@@ -84,81 +84,88 @@ def navigation_setup(self,par_obj):
     self.prev_file_btn.clicked.connect(lambda: self.Btn_fns.prev_file(par_obj))
     self.next_file_btn.clicked.connect(lambda: self.Btn_fns.next_file(par_obj))
 
+
 class btn_fn:
-    def __init__(self,win):
-        self.win=win
-        self.goto_img_fn=win.goto_img_fn
-    def prev_im(self,par_obj):
+    def __init__(self, win):
+        self.win = win
+        self.goto_img_fn = win.goto_img_fn
+
+    def prev_im(self, par_obj):
         if par_obj.curr_z > 0:
-            zslice = par_obj.curr_z-1
+            zslice = par_obj.curr_z - 1
             self.goto_img_fn(zslice=zslice)
 
-    def next_im(self,par_obj):
+    def next_im(self, par_obj):
         if par_obj.curr_z < par_obj.max_z:
-            zslice = par_obj.curr_z+1
+            zslice = par_obj.curr_z + 1
             self.goto_img_fn(zslice=zslice)
 
-    def last_im(self,par_obj):
+    def last_im(self, par_obj):
         zslice = par_obj.max_z
         self.goto_img_fn(zslice=zslice)
 
-    def first_im(self,par_obj):
+    def first_im(self, par_obj):
         zslice = 0
         self.goto_img_fn(zslice=zslice)
 
-    def prev_time(self,par_obj):
-        for idx,tpt in enumerate(par_obj.tpt_list):
+    def prev_time(self, par_obj):
+        for idx, tpt in enumerate(par_obj.tpt_list):
             if par_obj.curr_t == tpt:
-                if idx>0:
-                    tpt = par_obj.tpt_list[idx-1]
+                if idx > 0:
+                    tpt = par_obj.tpt_list[idx - 1]
                     self.goto_img_fn(tpt=tpt)
                     break
 
-    def next_time(self,par_obj):
-        if par_obj.max_t>par_obj.curr_t:
-            for idx,tpt in enumerate(par_obj.tpt_list):
+    def next_time(self, par_obj):
+        if par_obj.max_t > par_obj.curr_t:
+            for idx, tpt in enumerate(par_obj.tpt_list):
                 if par_obj.curr_t == tpt:
-                    if idx+1<len(par_obj.tpt_list):
-                        tpt = par_obj.tpt_list[idx+1]
+                    if idx + 1 < len(par_obj.tpt_list):
+                        tpt = par_obj.tpt_list[idx + 1]
                         self.goto_img_fn(tpt=tpt)
                         break
 
-    def prev_file(self,par_obj):
+    def prev_file(self, par_obj):
         if par_obj.curr_file > 0:
-            par_obj.max_z = min(par_obj.filehandlers[par_obj.curr_file-1].max_z,par_obj.user_max_z)
-            par_obj.max_t = min(par_obj.filehandlers[par_obj.curr_file-1].max_t,par_obj.tpt_list[-1])
-            if par_obj.curr_z>par_obj.max_z: par_obj.curr_z = par_obj.max_z
-            if par_obj.curr_t>par_obj.max_t: par_obj.curr_t = par_obj.max_t
-            self.goto_img_fn(imno=par_obj.curr_file-1)
-            #limit z by user and file extent
+            par_obj.max_z = min(par_obj.filehandlers[par_obj.curr_file - 1].max_z, par_obj.user_max_z)
+            par_obj.max_t = min(par_obj.filehandlers[par_obj.curr_file - 1].max_t, par_obj.tpt_list[-1])
+            if par_obj.curr_z > par_obj.max_z:
+                par_obj.curr_z = par_obj.max_z
+            if par_obj.curr_t > par_obj.max_t:
+                par_obj.curr_t = par_obj.max_t
+            self.goto_img_fn(imno=par_obj.curr_file - 1)
+            # limit z by user and file extent
 
-
-    def next_file(self,par_obj):
+    def next_file(self, par_obj):
         for fileno in range(par_obj.max_file):
             if fileno == par_obj.curr_file:
-                if fileno < par_obj.max_file-1:
-                    par_obj.max_z = min(par_obj.filehandlers[par_obj.curr_file+1].max_z,par_obj.user_max_z)
-                    par_obj.max_t = par_obj.filehandlers[par_obj.curr_file+1].max_t
-                    if par_obj.curr_z>par_obj.max_z: par_obj.curr_z = par_obj.max_z
-                    if par_obj.curr_t>par_obj.max_t: par_obj.curr_t = par_obj.max_t
-                    self.goto_img_fn(imno=par_obj.curr_file+1)
+                if fileno < par_obj.max_file - 1:
+                    par_obj.max_z = min(par_obj.filehandlers[par_obj.curr_file + 1].max_z, par_obj.user_max_z)
+                    par_obj.max_t = par_obj.filehandlers[par_obj.curr_file + 1].max_t
+                    if par_obj.curr_z > par_obj.max_z:
+                        par_obj.curr_z = par_obj.max_z
+                    if par_obj.curr_t > par_obj.max_t:
+                        par_obj.curr_t = par_obj.max_t
+                    self.goto_img_fn(imno=par_obj.curr_file + 1)
 
                     break
 
+
 def on_about(self):
     self.about_win = QtWidgets.QtWidget()
-    self.about_win.setWindowTitle('About QBrain Software v2.0')
+    self.about_win.setWindowTitle("About CytoCensus Software v2.0")
 
     license = return_license()
-    #with open (sys.path[0]+'/GNU GENERAL PUBLIC LICENSE.txt', "r") as myfile:
+    # with open (sys.path[0]+'/GNU GENERAL PUBLIC LICENSE.txt', "r") as myfile:
     #    data=myfile.read().replace('\n', ' ')
     #    license.append(data)
 
     # And give it a layout
     layout = QtWidgets.QVBoxLayout()
 
-    self.view = QtWebKit.QWebView()
-    self.view.setHtml('''
+    self.view = QtWidgets.QWebEngineView()
+    self.view.setHtml(
+        """
       <html>
 
 
@@ -166,33 +173,38 @@ def on_about(self):
           <form>
             <h1 >About</h1>
 
-            <p>Software written by Dominic Waithe and Martin Hailstone (c) 2015-2016</p>
-            '''+str(license)+'''
+            <p>Software written by Dominic Waithe and Martin Hailstone (c) 2015-2019</p>
+            """
+        + str(license)
+        + """
 
 
           </form>
         </body>
       </html>
-    ''')
+    """
+    )
     layout.addWidget(self.view)
     self.about_win.setLayout(layout)
     self.about_win.show()
     self.about_win.raise_()
 
+
 class checkBoxCH(QtWidgets.QCheckBox):
-    def __init__(self,par_obj,Win,ID,feature_select,text=None):
-        QtWidgets.QCheckBox.__init__(self,text)
+    def __init__(self, par_obj, Win, ID, feature_select, text=None):
+        QtWidgets.QCheckBox.__init__(self, text)
         self.stateChanged.connect(self.stateChange)
         self.feature_select = feature_select
-        self.ID=ID
-        self.Win=Win
-        self.par_obj=par_obj
+        self.ID = ID
+        self.Win = Win
+        self.par_obj = par_obj
+
     def stateChange(self):
-        par_obj=self.par_obj
-        Win=self.Win
+        par_obj = self.par_obj
+        Win = self.Win
         # set channels to use for feature calculation
-        if self.feature_select==True:
-            if self.isChecked()==True:
+        if self.feature_select == True:
+            if self.isChecked() == True:
                 if self.ID not in par_obj.ch_active:
                     par_obj.ch_active.append(self.ID)
                     par_obj.ch_active.sort()
@@ -200,21 +212,21 @@ class checkBoxCH(QtWidgets.QCheckBox):
                 if self.ID in par_obj.ch_active:
                     del par_obj.ch_active[par_obj.ch_active.index(self.ID)]
             par_obj.ch_display = par_obj.ch_active
-            
+
             if 0 in par_obj.filehandlers:
                 par_obj.numCH = par_obj.filehandlers[0].numCH
-                
+
             if par_obj.ex_img is not None:
                 newImg = return_rgb_slice(par_obj, 0, 0, 0)
-                '''
+                """
                 newImg = np.zeros((par_obj.ex_img.shape[0], par_obj.ex_img.shape[1], 3))
                 if par_obj.ch_active.__len__() > 1:
                     for ch_count, ch in enumerate(par_obj.ch_active):
                         if ch_count == 3: break
                         print (ch_count, ch)
                         newImg[:, :, ch_count] = img[:, :, ch]
-                '''
-                '''
+                """
+                """
                 if par_obj.ch_active.__len__() == 2:
                     print( par_obj.ch_active)
                     newImg = np.zeros((par_obj.ex_img.shape[0], par_obj.ex_img.shape[1], 3))
@@ -224,85 +236,85 @@ class checkBoxCH(QtWidgets.QCheckBox):
                     newImg = par_obj.filehandlers[0].get_tiff_slice([0], 0, range(par_obj.ex_img.shape[0]), range(par_obj.ex_img.shape[1]), par_obj.ch_active)
                 else:
                     newImg = par_obj.filehandlers[0].get_tiff_slice([0], 0, range(par_obj.ex_img.shape[0]), range(par_obj.ex_img.shape[1]), [par_obj.ch_active[:3]])
-                '''
-                Win.plt1.images[0].set_data(newImg/newImg.max())#/par_obj.tiffarraymax)
+                """
+                Win.plt1.images[0].set_data(newImg / newImg.max())  # /par_obj.tiffarraymax)
                 Win.canvas1.draw()
 
-        else: #set visible channels
-            #if self.ID not in par_obj.ch_display:
+        else:  # set visible channels
+            # if self.ID not in par_obj.ch_display:
             if self.isChecked():
                 if self.ID not in par_obj.ch_display:
                     par_obj.ch_display.append(self.ID)
                     par_obj.ch_display.sort()
-            else:#removes channel when unticked
+            else:  # removes channel when unticked
                 if self.ID in par_obj.ch_display:
                     del par_obj.ch_display[par_obj.ch_display.index(self.ID)]
-                    
-            displayed = 0 #counter for number of displayed channels
-            #Set only 3 displayed channels
+
+            displayed = 0  # counter for number of displayed channels
+            # Set only 3 displayed channels
             for ch, objset in enumerate(Win.ChannelGroup):
-                if ch in par_obj.ch_display and displayed<3:
+                if ch in par_obj.ch_display and displayed < 3:
                     Win.ChannelGroup[ch][2].showall()
                     Win.ChannelGroup[ch][4].showall()
-                    displayed = displayed+1
+                    displayed = displayed + 1
                 else:
                     Win.ChannelGroup[ch][2].hideall()
                     Win.ChannelGroup[ch][4].hideall()
-                        
-            Win.goto_img_fn(par_obj.curr_z,par_obj.curr_t,keep_roi=True)
+
+            Win.goto_img_fn(par_obj.curr_z, par_obj.curr_t, keep_roi=True)
+
 
 class contrast_controller(QtWidgets.QSlider):
-    def __init__(self,par_obj,Win,ID,brightness=False):
+    def __init__(self, par_obj, Win, ID, brightness=False):
         QtWidgets.QSlider.__init__(self)
-        self.ID=ID
-        self.Win=Win
-        self.par_obj=par_obj
+        self.ID = ID
+        self.Win = Win
+        self.par_obj = par_obj
         self.setTickPosition(0)
         self.setTickInterval(5)
         self.setMaximum(11)
         self.setMinimum(1)
         self.setMinimumWidth(50)
         self.setOrientation(QtCore.Qt.Horizontal)
-        if brightness==True:
+        if brightness == True:
             self.label = QtWidgets.QLabel(chr(9728))
-            self.setToolTip('Adjust Brightness')
+            self.setToolTip("Adjust Brightness")
             self.setMinimum(0)
             self.setMaximum(10)
             self.setValue(5)
-            self.setInvertedAppearance (True)
+            self.setInvertedAppearance(True)
             self.valueChanged.connect(self.change_brightness)
         else:
             self.label = QtWidgets.QLabel(chr(9681))
-            self.setToolTip('Adjust Contrast')
+            self.setToolTip("Adjust Contrast")
             self.valueChanged.connect(self.change_contrast)
 
-
-    def change_contrast(self,value):
+    def change_contrast(self, value):
         value = float(value)
-        self.par_obj.clim[self.ID][1]=value
+        self.par_obj.clim[self.ID][1] = value
         self.Win.goto_img_fn(keep_roi=True)
 
-    def change_brightness(self,value):
+    def change_brightness(self, value):
 
-        value = float(value)/10-0.5
-        print (self.ID)
-        self.par_obj.clim[self.ID][0]=value
+        value = float(value) / 10 - 0.5
+        print(self.ID)
+        self.par_obj.clim[self.ID][0] = value
 
         self.Win.goto_img_fn(keep_roi=True)
+
     def hideall(self):
         self.hide()
         self.label.hide()
+
     def showall(self):
         self.show()
         self.label.show()
-        
 
 
-
-def create_channel_objects(self,par_obj,num,feature_select=False,parent=None):
-    #Object factory for channel selection.
-    parent=[]
-    if hasattr(self,'CH_cbx'):
+def create_channel_objects(self, par_obj, num, feature_select=False, parent=None):
+    # Object factory for channel selection.
+    parent = []
+    if hasattr(self, "CH_cbx"):
         for cbx in self.CH_cbx:
             cbx.hide()
             cbx.destroy()
@@ -310,19 +322,19 @@ def create_channel_objects(self,par_obj,num,feature_select=False,parent=None):
     else:
         self.CH_cbx = []
 
-    for chID in range(0,num):
-        cbx=checkBoxCH(par_obj,self,chID,feature_select,'CH '+str(chID+1)+':')
+    for chID in range(0, num):
+        cbx = checkBoxCH(par_obj, self, chID, feature_select, "CH " + str(chID + 1) + ":")
 
         self.CH_cbx.append(cbx)
         self.CH_cbx[chID].setChecked(True)
 
-        if feature_select==False:
-            contrast=contrast_controller(par_obj,self,chID)
-            brightness=contrast_controller(par_obj,self,chID,brightness=True)
-            if chID>2:
+        if feature_select == False:
+            contrast = contrast_controller(par_obj, self, chID)
+            brightness = contrast_controller(par_obj, self, chID, brightness=True)
+            if chID > 2:
                 contrast.hideall()
                 brightness.hideall()
-            parent.append([cbx,contrast.label,contrast,brightness.label,brightness])
+            parent.append([cbx, contrast.label, contrast, brightness.label, brightness])
         else:
             parent.append([cbx])
 
