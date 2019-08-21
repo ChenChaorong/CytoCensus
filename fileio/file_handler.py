cc0af7cd3e9c89b6df8c6bea27466277f0f2152e
M <hailstonem@users.noreply.github.com>
2019-08-21 17:44:42 +0100

    Fix tifffile Z resolution loading

--- fileio/file_handler.py
+++ fileio/file_handler.py
@@ -1,14 +1,11 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
-import numpy as np
-from tifffile import TiffFile, imsave, TiffWriter#Install with pip install tifffile.
 import os
+import numpy as np
+from tifffile import TiffFile, imsave, TiffWriter  # Install with pip install tifffile.
 """
 Created on Fri Feb 17 19:48:58 2017
-
 @author: martin
-"""
-'''
 Since it's clear that we want to handle files with all their own data in a
 coherent way
 And at the moment we rely on 'Parameter Object' effectively global parameters
@@ -41,152 +38,171 @@ The former seems neater.
 The file handlers themselves can be a list as part of par_obj, thus allowing
 easy pass of information between them.
 Theoretically it might be nice to separate them entirely, but I suspect this
-would cause problems without some significant rewrites
-'''
+would cause problems without some significant rewrites"""
 
-
-#create new file_handler for each file
 class File_handler(object):
-    def __init__(self,file_path):
-        #store high level file data and metadata
-        self.full_name=file_path
+    """create new File_handler object for each file"""
 
-        self.base_name=os.path.basename(file_path)
-        self.path=os.path.dirname(file_path)
+    def __init__(self, file_path):
+        # store high level file data and metadata
+        self.full_name = file_path
+
+        self.base_name = os.path.basename(file_path)
+        self.path = os.path.dirname(file_path)
 
         self.ext = self.base_name.split(".")[-1]
         self.name = self.base_name.split(".")[0]
 
-        self.array = [] #memmap object l
+        self.array = []  # memmap object l
         self.z_calibration = 1
-        self.order={} #ordering of tiff objects
-        #default file extents
+        self.order = {}  # ordering of tiff objects
 
+        # default file extents
+        self.height = 0
+        self.width = 0
         self.max_t = 0
         self.max_z = 0
         self.numCH = 0
-
+        self.tiff = None
+        self.tiffarray_typemax = 0  # dtype max
+        self.tiffarraymax = 0  # actual data maximum
+        self.bitDepth = 0
         self.import_file()
 
-    def get_tiff_slice(self,tpt=[0],zslice=[0],x=[0],y=[0],c=[0]):
-        #deal with different TXYZC orderings. Always return TZYXC
-        #handles lists and ints nicely
-        if type(zslice) is not list: zslice = [zslice]
-        if type(zslice[0]) is list: zslice = zslice[0]
+    def get_tiff_slice(self, tpt=[0], zslice=[0], x=[0], y=[0], c=[0]):
+        """Get image cube using numpy views, dealing with different TXYZC orderings. Always return TZYXC"""
+        # handles lists and ints nicely
+        if not isinstance(zslice, list):  # type(zslice) is not list:
+            zslice = [zslice]
+        if isinstance(zslice[0], list):
+            zslice = zslice[0]
 
-        alist=[]
-        blist=[]
-        for n,b in enumerate(self.order):
-            if b=='T':
+        alist = []
+        blist = []
+        for n, axis in enumerate(self.order):
+            if axis == "T":
                 alist.append(tpt)
                 blist.append(n)
-        for n,b in enumerate(self.order):
-            if b=='Z':
+        for n, axis in enumerate(self.order):
+            if axis == "Z":
                 alist.append(zslice)
                 blist.append(n)
-        for n,b in enumerate(self.order):
-            if b=='Y':
+        for n, axis in enumerate(self.order):
+            if axis == "Y":
                 alist.append(y)
                 blist.append(n)
-        for n,b in enumerate(self.order):
-            if b=='X':
+        for n, axis in enumerate(self.order):
+            if axis == "X":
                 alist.append(x)
                 blist.append(n)
 
-        for n,b in enumerate(self.order):
-            if b=='C' or b=='S':
+        for n, axis in enumerate(self.order):
+            if axis == "C" or axis == "S":
                 alist.append(c)
                 blist.append(n)
 
-        tiff2=self.array.transpose(blist)
+        tiff2 = self.array.transpose(blist)
 
-        if self.order.__len__()==5:
-            #tiff=np.squeeze(tiff2[alist[0],:,:,:,:][:,alist[1],:,:,:][:,:,alist[2],:,:][:,:,:,alist[3],:][:,:,:,:,alist[4]])
-            tiff=np.squeeze(tiff2[np.ix_(alist[0],alist[1],alist[2],alist[3],alist[4])])
-        elif self.order.__len__()==4:
-            #tiff=np.squeeze(tiff2[alist[0],:,:,:][:,alist[1],:,:][:,:,alist[2],:][:,:,:,alist[3]])
-            tiff=np.squeeze(tiff2[np.ix_(alist[0],alist[1],alist[2],alist[3])])
+        if self.order.__len__() == 5:
+            # tiff=np.squeeze(tiff2[alist[0],:,:,:,:][:,alist[1],:,:,:][:,:,alist[2],:,:][:,:,:,alist[3],:][:,:,:,:,alist[4]])
+            tiff = np.squeeze(tiff2[np.ix_(alist[0], alist[1], alist[2], alist[3], alist[4])])
+        elif self.order.__len__() == 4:
+            # tiff=np.squeeze(tiff2[alist[0],:,:,:][:,alist[1],:,:][:,:,alist[2],:][:,:,:,alist[3]])
+            tiff = np.squeeze(tiff2[np.ix_(alist[0], alist[1], alist[2], alist[3])])
 
-        elif self.order.__len__()==3:
-            #tiff=np.squeeze(tiff2[alist[0],:,:][:,alist[1],:][:,:,alist[2]])
-            tiff=np.squeeze(tiff2[np.ix_(alist[0],alist[1],alist[2])])
-        elif self.order.__len__()==2:
-            #tiff=np.squeeze(tiff2[alist[0],:][:,alist[1]])
-            tiff=np.squeeze(tiff2[np.ix_(alist[0],alist[1])])
+        elif self.order.__len__() == 3:
+            # tiff=np.squeeze(tiff2[alist[0],:,:][:,alist[1],:][:,:,alist[2]])
+            tiff = np.squeeze(tiff2[np.ix_(alist[0], alist[1], alist[2])])
+        elif self.order.__len__() == 2:
+            # tiff=np.squeeze(tiff2[alist[0],:][:,alist[1]])
+            tiff = np.squeeze(tiff2[np.ix_(alist[0], alist[1])])
         return tiff
 
     def close(self):
-        self.array =[]
-        self.Tiff.close()
+        self.array = []
+        self.tiff.close()
 
     def import_file(self):
+        """ loads in File image data for subsequent use. Currently only supports TIFF"""
+        # reworked to separate file logic from UI related logic
+        # TODO add directory separation logic
 
-        #loads in Tiff image data for subsequent use
-        #reworked to separate file logic from UI related logic
-        #TODO add directory separation logic
-
-
-        if self.ext == 'tif' or self.ext == 'tiff':
+        if self.ext == "tif" or self.ext == "tiff":
             self.import_tiff()
-            return True, 'Image loaded'
+            return True, "Image loaded"
         else:
-            statusText = 'Status: Image format not-recognised. Please choose either png or TIFF files.'
-            return False, statusText
+            status_text = "Status: Image format not-recognised. Please choose TIF/TIFF files."
+            return False, status_text
 
     def import_tiff(self):
-            self.Tiff = TiffFile(self.full_name)
+        tiff = TiffFile(self.full_name)
+        self.tiff = tiff
 
-            meta = self.Tiff.series[0]
+        meta = tiff.series[0]
+        if tiff.is_imagej:
+            try:  # if an imagej file, we know where, and can extract the x,y,z resolutions
+                x_res = tiff.pages[0].tags["XResolution"].value
+                y_res = tiff.pages[0].tags["YResolution"].value
 
-            try: #if an imagej file, we know where, and can extract the x,y,z
-            #if 1==1:
-                x = self.Tiff.pages[0].tags.x_resolution.value
-                y = self.Tiff.pages[0].tags.y_resolution.value
-                if x!=y: raise Exception('x resolution different to y resolution')# if this isn't true then something is wrong
-                x_res=float(x[1])/float(x[0])
+                # X resolution stored as Fraction
+                x_res = float(x_res[1]) / float(x_res[0])
 
-                z=self.Tiff.pages[0].imagej_tags['spacing']
+                z_res = tiff.imagej_metadata["spacing"]  # .pages[0].imagej_tags['spacing']
 
-                self.z_calibration = z/x_res
+                # We're interested in X resolution relative to Z
+                self.z_calibration = z_res / x_res
 
-                print('z_scale_factor', self.z_calibration)
-            except:
-                #might need to modify this to work with OME-TIFFs
-                print ('tiff resolution not recognised')
+                print("z_scale_factor", self.z_calibration)
+            except (AttributeError, KeyError) as ex:
+                print("tiff resolution not recognised")
+        elif self.tiff.is_ome:
+            try:
+                x_res = tiff.ome_metadata["Image"]["Pixels"]["PhysicalSizeX"]
+                print(x_res)
+                y_res = tiff.ome_metadata["Image"]["Pixels"]["PhysicalSizeY"]
+                z_res = tiff.ome_metadata["Image"]["Pixels"]["PhysicalSizeZ"]
+                # We're interested in X resolution relative to Z
+                self.z_calibration = z_res / x_res
 
-            self.order = meta.axes
-            for n,b in enumerate(self.order):
-                    if b == 'T':
-                        self.max_t = meta.shape[n]-1
-                    if b == 'Z':
-                        self.max_z = meta.shape[n]-1
-                    if b == 'Y':
-                        self.height = meta.shape[n]
-                    if b == 'X':
-                        self.width = meta.shape[n]
-                    if b == 'S':
-                        self.numCH = meta.shape[n]
-                        #par_obj.tiff_reorder=False
-                    if b == 'C':
-                        self.numCH = meta.shape[n]
+                print("z_scale_factor", self.z_calibration)
+            except (AttributeError, KeyError) as ex:
+                print("tiff resolution not recognised")
 
-            self.bitDepth = meta.dtype
+        self.order = meta.axes
+        for n, axis in enumerate(self.order):
+            if axis == "T":
+                self.max_t = meta.shape[n] - 1
+            if axis == "Z":
+                self.max_z = meta.shape[n] - 1
+            if axis == "Y":
+                self.height = meta.shape[n]
+            if axis == "X":
+                self.width = meta.shape[n]
+            if axis == "S":  # RGB image colour channel
+                self.numCH = meta.shape[n]
+                # par_obj.tiff_reorder=False
+            if axis == "C":
+                self.numCH = meta.shape[n]
 
-            self.array=self.Tiff.asarray()#memmap=True)
+        self.bitDepth = meta.dtype
 
-            self.tiffarraymax = self.array.max()
+        self.array = self.tiff.asarray()  # memmap=True)
 
-            if self.bitDepth in ['uint8','uint16']:
-                self.tiffarray_typemax=np.iinfo(self.bitDepth).max
-                #12 bit depth
-                if self.bitDepth in ['uint16'] and self.tiffarraymax<4096:
-                    self.tiffarray_typemax=4095          
-            else:
-                self.tiffarray_typemax=np.finfo(self.bitDepth).max
+        self.tiffarraymax = self.array.max()
 
-class Intermediate_handler():
+        if self.bitDepth in ["uint8", "uint16"]:
+            self.tiffarray_typemax = np.iinfo(self.bitDepth).max
+            # 12 bit depth
+            if self.bitDepth in ["uint16"] and self.tiffarraymax < 4096:
+                self.tiffarray_typemax = 4095
+        else:
+            self.tiffarray_typemax = np.finfo(self.bitDepth).max
+
+
+class Intermediate_handler:
     """ Wrapper for saving intermediate results to file and keeping track of them """
-    def  __init__(self,filename):
+
+    def __init__(self, filename):
         try:
             os.remove(filename)
         except:
@@ -196,54 +212,52 @@ class Intermediate_handler():
         self.filename = filename
         self.refs = []
         self.array = None
+
     def reader(self):
         self.read = True
         self.write = False
         return self
-        
+
     def writer(self):
         self.write = True
         self.read = False
         return self
-        
+
     def __enter__(self):
         if self.write:
-            self.tif = TiffWriter(self.filename, bigtiff=True,append=True)
+            self.tif = TiffWriter(self.filename, bigtiff=True, append=True)
         if self.read:
             self.tif = TiffFile(self.filename)
-            self.array = self.tif.asarray()#memmap=True)
+            self.array = self.tif.asarray()  # memmap=True)
 
         return self
 
     def __exit__(self, exc_type, exc_value, traceback):
-            self.array = None
-            self.tif.close()
-                
+        self.array = None
+        self.tif.close()
 
     def close(self):
-            self.array = None
-            
-            if self.tif is not None:
-                self.tif.close()
+        self.array = None
 
-    def write_plane(self, data,t,z,f):
-        #works plane by plane
-        #print 'writing plane' + str((t,z,f))
-        #print data.shape
-            
-        self.tif.save(data, compress = 0, contiguous = True)
-        
-        if len(data.shape)>2:
-            for i in range (data.shape[0]):
+        if self.tif is not None:
+            self.tif.close()
+
+    def write_plane(self, data, t, z, f):
+        # works plane by plane
+        # print 'writing plane' + str((t,z,f))
+        # print data.shape
+
+        self.tif.save(data, compress=0, contiguous=True)
+
+        if len(data.shape) > 2:
+            for i in range(data.shape[0]):
                 self.plane += 1
-                self.refs = self.refs + [(t,z+i)]
+                self.refs = self.refs + [(t, z + i)]
         else:
             self.plane += 1
-            self.refs = self.refs + [(t,z)]
-    
-    def read_plane(self,t,z,f):
-        #works plane by plane
-        planeno = self.refs.index((t,z))
+            self.refs = self.refs + [(t, z)]
+
+    def read_plane(self, t, z, f):
+        # works plane by plane
+        planeno = self.refs.index((t, z))
         return np.squeeze(self.tif.asarray(planeno))
-        
-        
